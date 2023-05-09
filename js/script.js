50ca5482b0475a820d21172abf6c5ce0b49aae8e
Andriy Koval <akoval29@gmail.com>
2023-05-09 06:16:14 +0300

    import-export

--- js/script.js
+++ js/script.js
@@ -1,67 +1,22 @@
-const cityInput = document.querySelector(".inputBlock__input");
-const selected = document.querySelector(".inputBlock__selected");
-const searchBtn = document.querySelector(".inputBlock__go");
+import { getWeatherTop } from "./useAPI.js";
+import { getWeatherBottom } from "./useAPI.js";
 
+const main = document.querySelector(".main");
+export const cityInput = document.querySelector(".inputBlock__input");
+const selected = document.querySelector(".inputBlock__selected");
+const searchBtn = document.querySelector(".inputBlock__btn");
 const outTopStart = document.querySelector(".outputBlock__start");
 const outTopMid = document.querySelector(".outputBlock__mid");
 const outTopEnd = document.querySelector(".outputBlock__end");
-
 const outBottom = document.querySelector(".outputBlock__bottom");
 
-let lastCity = null; // уникнути повторних запитів на сервер 1/3
-async function getWeather(requestType, city) {
-  const apiKey = "8018cfbdc94ba8e035065ccbf8eec87c";
-
-  if (!city || (lastCity && lastCity === city)) {
-    return;
-  } // уникнути повторних запитів на сервер 2/3
-
-  try {
-    const response = await axios.get(
-      `http://api.openweathermap.org/data/2.5/${requestType}?q=${city}&appid=${apiKey}`
-    );
-
-    onRequest(response.data);
-    lastCity = city; // уникнути повторних запитів на сервер 3/3
-    cityInput.value = "";
-  } catch (error) {
-    onError();
-    console.error(error);
-    throw error;
-  }
-}
-
-// перший старт - Київ
-document.addEventListener("DOMContentLoaded", () => {
-  getWeather("weather", "kyiv");
-});
-
-// пошук із значення в інпуті
-searchBtn.addEventListener("click", () => {
-  const city = cityInput.value;
-  getWeather("weather", city);
-});
-
-// помилка
-function onError() {
-  cityInput.value = "ERROR";
-  cityInput.style.fontWeight = "bold";
-  cityInput.style.backgroundColor = "red";
-  setTimeout(() => {
-    cityInput.style.removeProperty("background-color");
-    cityInput.value = "";
-  }, 1000);
-}
-
-function onRequest(data) {
+function onRequestTop(data) {
   const temperature = Math.round(data.main.temp - 273.15);
   const temperatureFeels = Math.round(data.main.feels_like - 273.15);
   const cityName = data.name;
   const countryName = data.sys.country;
   const weatherIcon = data.weather[0].icon;
   const weatherDescr = data.weather[0].description;
-
-  console.log(data);
 
   selected.innerHTML = `Selected: ${cityName}, ${countryName}`;
 
@@ -76,4 +31,52 @@ function onRequest(data) {
   `;
 
   outTopEnd.innerHTML = `<img class="outputBlock__img" src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="weather icon">`;
+}
+
+// function onRequestBottom(data) {
+//   console.log("bottom");
+//   console.log(resBottom.data);
+// }
+
+// перший старт - Київ
+document.addEventListener("DOMContentLoaded", async () => {
+  const resTop = await getWeatherTop("kyiv");
+  onRequestTop(resTop.data);
+  console.log("top");
+  console.log(resTop.data);
+
+  const resBottom = await getWeatherBottom("kyiv");
+  // onRequestBottom(resBottom.data);
+  console.log("bottom");
+  console.log(resBottom.data);
+});
+
+// пошук
+searchBtn.addEventListener("click", () => {
+  // getWeatherTop(cityInput.value);
+  // getWeatherBottom(cityInput.value);
+});
+
+// інпут реагує на клавішу ENTER
+cityInput.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchBtn.click();
+  }
+});
+
+// фокус на текстовий інпут при кліках по сторінці
+main.addEventListener("click", () => {
+  cityInput.focus();
+});
+
+// помилка
+export function onError() {
+  cityInput.value = "ERROR";
+  cityInput.style.fontWeight = "bold";
+  cityInput.style.backgroundColor = "red";
+  setTimeout(() => {
+    cityInput.style.removeProperty("background-color");
+    cityInput.value = "";
+  }, 1000);
 }
---
+++ js/useAPI.js
@@ -0,0 +1,44 @@
+import { onError } from "./script.js";
+import { cityInput } from "./script.js";
+
+const apiKey = "8018cfbdc94ba8e035065ccbf8eec87c";
+let lastCity1 = null; // уникнути повторних запитів на сервер 1/3
+
+// top block
+export async function getWeatherTop(city) {
+  if (!city || (lastCity1 && lastCity1 === city)) {
+    return;
+  } // уникнути пустих і повторних запитів на сервер 2/3
+  try {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
+    lastCity1 = city; // уникнути повторних запитів на сервер 3/3
+    cityInput.value = "";
+    return response;
+  } catch (error) {
+    onError();
+    console.error(error);
+    throw error;
+  }
+}
+
+// bottom block
+let lastCity2 = null; // уникнути повторних запитів на сервер 1/3
+export async function getWeatherBottom(city) {
+  if (!city || (lastCity2 && lastCity2 === city)) {
+    return;
+  } // уникнути пустих і повторних запитів на сервер 2/3
+  try {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    );
+    lastCity2 = city; // уникнути повторних запитів на сервер 3/3
+    cityInput.value = "";
+    return response;
+  } catch (error) {
+    onError();
+    console.error(error);
+    throw error;
+  }
+}
